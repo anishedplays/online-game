0cea7b926aba9fb8a041faf0a748ac60dd36adb5
anishedplays <74805469+anishedplays@users.noreply.github.com>
2023-06-09 22:45:07 +0530

    wtf

--- network.py
+++ network.py
@@ -1,16 +1,18 @@
 import socket
+import pickle
 
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.43.65"
-        self.port = 90
+        self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
+
     def connect(self):
         try:
             self.client.connect(self.addr)
@@ -21,6 +23,6 @@ class Network:
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048*2))
         except socket.error as e:
             print(e)
