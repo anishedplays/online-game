9a398e27db35741b3098628017e253c0eb555255
anishedplays <74805469+anishedplays@users.noreply.github.com>
2023-06-09 22:25:03 +0530

    ff

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import pygame
-from network import network
+from network import Network
 
 width = 500
 height = 500
@@ -37,22 +37,25 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
-    self.update()
+        self.update()
+
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
+
+
 def read_pos(str):
     str = str.split(",")
     return int(str[0]), int(str[1])
 
 
-def make_pos(tup)
+def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
 
 def redrawWindow(win,player, player2):
     win.fill((255,255,255))
-    player2.draw(win)
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
@@ -60,24 +63,23 @@ def main():
     run = True
     n = Network()
     startPos = read_pos(n.getPos())
-    p = Player(startPos[0],100,100,(0,255,0))
-    p2 = Player(0,0,100,100,(0,255,0))
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
+    p2 = Player(0,0,100,100,(255,0,0))
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-        p2Pos = read_pos(n.send(make_pos((p.x, p.x))))
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
         p2.x = p2Pos[0]
         p2.y = p2Pos[1]
         p2.update()
 
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
         p.move()
-        redrawWindow(win, p)
+        redrawWindow(win, p, p2)
 
 main()
